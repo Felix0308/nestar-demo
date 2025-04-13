b378923424a0eb0f199895ebd11138b292fcc2d7
Felix0308 <utkirbekm.0327@gmail.com>
2025-04-13 15:06:35 +0900

    feat: MUI customized integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,27 +1,27 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  console.log("DOCUMENT = PAGE ROUTER");
-
   return (
     <Html lang="en">
       <Head>
         <meta charSet="UTF-8" />
-        <meta name="title" content="Nestar, Demo" />
-        <meta name="robots" content="index, follow" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index,follow" />
         <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
         {/* SEO */}
         <meta
           name="keyword"
-          content="nestar, nestar.uz, devex mern, mern nestjs fullstack"
+          content={"nestar, nestar.uz, devex mern, mern nestjs fullstack"}
         />
         <meta
           name="desctiption"
           content={
-            "But and sell properties anywhere in South Korea. | " +
-            "하지만 한국 어디에서나 부동산을 판매하세요"
+            "Buy and sell properties anywhere anytime in South Korea. | " +
+            "Покупайте и продавайте недвижимость в любой точке Южной Кореи в любое время." +
+            ""
           }
-        />
+        ></meta>
       </Head>
       <body>
         <Main />
