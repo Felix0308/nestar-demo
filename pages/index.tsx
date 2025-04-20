08496040e5e6fa766186ebc8e11aafe74ecb90ed
Felix0308 <utkirbekm.0327@gmail.com>
2025-04-20 15:04:46 +0900

    feat: develop apollo client connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,43 @@
-import { NextPage } from "next";
+import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import { Stack } from "@mui/material";
 import TrendProperties from "@/libs/components/homepage/TrendProperties";
+import { NextPage } from "next";
 import PopularProperties from "@/libs/components/homepage/PopularProperties";
 import Advertisement from "@/libs/components/homepage/Advertisement";
 import TopProperties from "@/libs/components/homepage/TopProperties";
 import TopAgents from "@/libs/components/homepage/TopAgents";
-import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import "swiper/css";
-import "swiper/css/pagination";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTIES } from "@/apollo/user/query";
 
 const Home: NextPage = () => {
   const device = useDeviceDetect();
+
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 5,
+        sort: "createdAt",
+        direction: "DESC",
+        search: {
+          // typeList: ["APARTMENT"],
+          // locationList: ["SEOUL"],
+        },
+      },
+    },
+  });
+
+  console.log("getPropertiesData", getPropertiesData);
 
   if (device === "mobile") {
     return <Stack>HOMEPAGE MOBILE</Stack>;
